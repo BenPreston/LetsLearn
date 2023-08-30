06680a01a6105a563bd441999219b180ff0ded12
Ben Preston <ben.m.preston@gmail.com>
2023-08-30 10:28:27 +0100

    fix: added up to data table

--- app/course-selector/page.tsx
+++ app/course-selector/page.tsx
@@ -1,3 +1,15 @@
+import { DataTable } from "@/components/ui/dataTable";
+import WithNavsLayout from "@/components/templates/WithNavsLayout";
+
 export default function CourseSelector() {
-  return <div>CourseSelector Page</div>;
+  return (
+    <WithNavsLayout>
+      <div className="p-6">
+        <h2 className="mb-6 text-subtitle font-display">
+          What do you want to learn?
+        </h2>
+        <DataTable />
+      </div>
+    </WithNavsLayout>
+  );
 }
